132e6a0b1ffce419dc2d275cff731e93d208fc8c
DisappointmentIncarnate <112603794+DisappointmentIncarnate@users.noreply.github.com>
2024-03-17 13:10:08 -0400

    caterpillar sprite update based on health
    increased base speed
    when getting unhealthy, reduce speed

--- Assets/Scripts/Caterpillar.cs
+++ Assets/Scripts/Caterpillar.cs
@@ -9,14 +9,15 @@ public class Caterpillar : MonoBehaviour
     public int health; //default 4 (contributes to player speed, at 0 the player loses)
     public float speed; //0.01 is the float used in the first scene
     public float jump; //17 is the default used in the first scene
+    public Sprite[] healthSprites;
     private float horizontalMovement;
     Rigidbody2D rigidBody;
-    SpriteRenderer sprite;
+    SpriteRenderer spriteRender;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
-        sprite = GetComponent<SpriteRenderer>();
+        spriteRender = GetComponent<SpriteRenderer>();
         hitpoints = 3;
         health = 4;
 
@@ -37,9 +38,9 @@ public class Caterpillar : MonoBehaviour
         gameObject.transform.position = new Vector2(transform.position.x + (horizontalMovement * speed), transform.position.y ); //transform x based upon direction & speed
 
         if(horizontalMovement == -1){ //if going left, flip sprite
-            sprite.flipX = true;
+            spriteRender.flipX = true;
         }else if (horizontalMovement == 1){ //else do not flip sprite
-            sprite.flipX = false;
+            spriteRender.flipX = false;
         }
     }
     
@@ -47,6 +48,7 @@ public class Caterpillar : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space) && rigidBody.velocity.y == 0){ //if space pressed & if the player is not moving up or down (jumping or falling)
             rigidBody.AddForce(transform.up * jump, ForceMode2D.Impulse); //add upward force to the rigidbody2d, using impulse force
         }
+        spriteUpdate();
     }
 
     void OnCollisionEnter2D(Collision2D col){ //on collision with anything
@@ -71,17 +73,22 @@ public class Caterpillar : MonoBehaviour
                 hitpoints = hitpoints - damage;
             }else if(stat == 1){ //reduce health
                 health = health - damage;
+                speed-=0.03f;
             }
         }else{
             if(stat == 0 && hitpoints < 3){ //increase hit points
                 hitpoints = hitpoints + damage;
             }else if(stat == 1 && health < 4){ //increase health
                 health = health + damage;
+                speed+=0.03f;
             }
         }
         if(health <= 0 || hitpoints <= 0){ //player dies from damage
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
-        print("HP: " + hitpoints + " Health: " + health); //temporary console print
+    }
+
+    void spriteUpdate(){
+        spriteRender.sprite = healthSprites[health];
     }
 }
