a31b71bf56d3821f98b5cbc4f5482c6d9c794134
DisappointmentIncarnate <112603794+DisappointmentIncarnate@users.noreply.github.com>
2024-03-17 18:44:03 -0400

    Item script can now change texture

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
+    public Sprite[] goodItems;
+    public Sprite[] badItems;
+    SpriteRenderer spriteRender;
+
     void OnCollisionEnter2D(Collision2D col){
         if(col.gameObject.tag == "Player"){
             Destroy(this.gameObject);
+        }
+    }
+
+    void Start(){
+        spriteRender = GetComponent<SpriteRenderer>();
+        if (gameObject.tag == "bad_item"){
+            spriteRender.sprite = badItems[Random.Range(0, badItems.Length)]; //random item from list of items
+            gameObject.transform.localScale += new Vector3(2.5f,2.5f,0); //since we're using 32x32 textures, scale up
         }
     }
 }
